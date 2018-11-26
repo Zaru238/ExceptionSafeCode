9e46d3d6fe932957d7e41023ee2dcd0f3696a438
Kostiantyn Ziabrev <konstantin.ziabrev@gmail.com>
2018-11-26 23:11:49 +0200

    Script fix

--- tools/clang_format_check.sh
+++ tools/clang_format_check.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 find -name "*.h" -o -name "*.hpp" -o -name "*.c" -o -name "*.cpp" |
-xargs clang-format-7 -style=file -output-replacements-xml |
+xargs clang-format -style=file -output-replacements-xml |
 
 grep "<replacement " >/dev/null
 if [ $? -ne 1 ]; then
-	echo "Commit didn't pass clang-format check"
-	echo "Please run scripts/apply_clang_format.sh for formatting (run from project dir)"
-	exit 1
+  echo "Commit didn't pass clang-format check"
+  echo "Please run scripts/apply_clang_format.sh for formatting (run from project dir)"
+  exit 1
+else
+  echo "Clang format check have successfully passed"
 fi
 
 exit
